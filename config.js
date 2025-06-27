ef5c52a2d5a7496b1161e876abb4544e1ed6a431
royizidon <zidonr@gmail.com>
2025-06-27 14:05:08 +0300

    v7

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const LANDMARKS_URL = 'https://api.jewishatlas.org/api/landmarks';
-export { LANDMARKS_URL };
-
+// config.js
+// config.js
+window.LANDMARKS_URL = 'https://api.jewishatlas.org/api/landmarks';
