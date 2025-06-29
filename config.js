b20b8b4fbd7a2a701fea9e5da600a3c43d94fa18
royizidon <zidonr@gmail.com>
2025-06-29 22:30:59 +0300

    v9

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // config.js
-// config.js
-// config.js
-window.LANDMARKS_URL = "https://api.jewishatlas.org/api/landmarks/";
+// 1) the real ArcGIS REST service (so your templateFL can grab renderer + popups)
+window.LANDMARKS_SERVICE_URL = "https://services-eu1.arcgis.com/FckSU1kja7wbnBnq/arcgis/rest/services/Landmarks/FeatureServer/0";
+// 2) your proxy endpoint (for fetching sampled JSON)
+window.LANDMARKS_PROXY_URL   = "https://api.jewishatlas.org/api/landmarks";
