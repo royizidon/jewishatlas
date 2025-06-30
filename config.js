4d5f7fc7d34a668b320310f39850c671319542c4
royizidon <zidonr@gmail.com>
2025-06-30 06:38:49 +0300

    de

--- config.js
+++ config.js
@@ -3,3 +3,6 @@
 window.LANDMARKS_SERVICE_URL = "https://services-eu1.arcgis.com/FckSU1kja7wbnBnq/arcgis/rest/services/Landmarks/FeatureServer/0";
 // 2) your proxy endpoint (for fetching sampled JSON)
 window.LANDMARKS_PROXY_URL   = "https://api.jewishatlas.org/api/landmarks";
+
+window.LANDMARKS_SERVICE_URL = "https://.../FeatureServer/0";
+window.LANDMARKS_PROXY_URL = "https://api.jewishatlas.org/api/landmarks";
