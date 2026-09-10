a52e03a9d781847eb12884b58879942c335c8bb7
royizidon <zidonr@gmail.com>
2026-09-10 15:07:45 +0300

    new map

--- dedicate.js
+++ dedicate.js
@@ -508,10 +508,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     _leafletMap = L.map("dedicateMap", { zoomControl: true }).setView([32, 20], 3);
 
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-      attribution: "© OpenStreetMap, © CARTO",
-      subdomains: "abcd",
-      maxZoom: 19
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19
     }).addTo(_leafletMap);
 
     // Custom pin icon matching the site's indigo style
